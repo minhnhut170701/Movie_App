e4b390d0c7600200fefa347a705989f667a69ee8
minhnhut170701 <minhnhut170701@gmail.com>
2022-05-01 09:06:10 +0700

    edit about component

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,25 +1,29 @@
 import { ItemAbout } from './ItemAbout';
 import React from 'react'
+import about from "../../images/about-01.png";
+import icon1 from "../../images/icon01.png";
+import icon2 from "../../images/icon02.png";
+import icon3 from "../../images/icon03.png";
 
 const About = () => {
   return (
     <div className='text-white max-w-7xl margin-center mt-24'>
        <div role="article" className='flex items-center space-x-10'>
            <img className='w-[50%]'
-           src='http://digiflex.themezinho.net/wp-content/uploads/2020/12/side-image01.png' alt="error" />
+           src={about} alt="banner" />
            <div className='space-y-20'>
                <h1 className='text-5xl font-bold'>Best pick for hassle-free streaming experience.</h1>
                {/* line 1 */}
                <ItemAbout  
-               img="http://digiflex.themezinho.net/wp-content/uploads/2020/11/icon01.png"
+               img={icon1}
                title="Access while traveling" 
                des="Keep access to your entertainment content while roaming the world.Pick from thousands."   />
                <ItemAbout  
-               img="http://digiflex.themezinho.net/wp-content/uploads/2020/11/icon02.png"
+               img={icon2}
                title="Stream with no interruptions" 
                des="Pause for snacks, not buffering. Stream smoothly with our lightning-fast NordLynx protocol network."   />
                <ItemAbout  
-               img="http://digiflex.themezinho.net/wp-content/uploads/2020/11/icon03.png"
+               img={icon3}
                title="Stay secure at all times" 
                des="Securely access and enjoy your favorite content, even on public Wi-Fi. Your connection"   />
            </div>
